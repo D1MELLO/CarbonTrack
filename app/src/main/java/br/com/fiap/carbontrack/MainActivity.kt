f5623107ae4f40e07f76e66db23059ba9447460f
D1MELLO <sergio_dimello@outlook.com>
2025-03-15 23:22:37 -0300

    Primeiro Hello World

--- app/src/main/java/br/com/fiap/carbontrack/MainActivity.kt
+++ app/src/main/java/br/com/fiap/carbontrack/MainActivity.kt
@@ -3,24 +3,23 @@ package br.com.fiap.carbontrack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import br.com.fiap.carbontrack.screens.LoginScreen
 import br.com.fiap.carbontrack.ui.theme.CarbonTrackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CarbonTrackTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    LoginScreen()
                 }
             }
         }
--- app/src/main/java/br/com/fiap/carbontrack/screens/LoginScreen.kt
+++ app/src/main/java/br/com/fiap/carbontrack/screens/LoginScreen.kt
@@ -1,2 +1,23 @@
 package br.com.fiap.carbontrack.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Hello World, I'm CarbonTrack!"
+            )
+        }
+    }
+}
